2a9448d2bfe4802b8262c31b6cae91b3dfad705c
Jan-Willem Oostendorp <webmaster@janw.me>
2026-03-20 19:01:06 +0100

    Add basic ansible server.

--- app/Jobs/RunAnsible.php
+++ app/Jobs/RunAnsible.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\Server;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Process;
@@ -15,37 +16,33 @@ class RunAnsible implements ShouldQueue
      */
     public function __construct(
         public int $serverID
-    )
-        {
-            // This should whhen a new site is created.
-            // Pass on the prim ID.
-    }
+    ) {}
 
     /**
      * Execute the job.
-    */
+     */
     public function handle(): void
     {
-        $server = \App\Models\Server::findOrFail( $this->serverID );
+        $server = Server::firstOrFail($this->serverID);
 
-        $this->testRawSSHConnection( $server );
+        $this->testRawSSHConnection($server);
     }
 
     /**
      * Test the raw SSH connection
      *
-     * @param mixed $server
+     * @param  mixed  $server
      */
-    protected function testRawSSHConnection( $server) {
+    protected function testRawSSHConnection($server)
+    {
         $user = 'root'; // @todo make this a variable.
         $port = 22; // @todo make this a variable.
         $server_ip = $server->ip_address;
         $server_name = $server->name;
         $private_key = $server->ssh_private_key;
 
-
         // Put the private key in a temporary file
-        $tempKeyPath = tempnam(sys_get_temp_dir(), 'ssh_key_' . $server_name);
+        $tempKeyPath = tempnam(sys_get_temp_dir(), 'ssh_key_'.$server_name);
         file_put_contents($tempKeyPath, $private_key);
         chmod($tempKeyPath, 0600); // Set permissions to read/write for the owner only
 
@@ -53,10 +50,10 @@ class RunAnsible implements ShouldQueue
         $result = Process::run(
             "ssh $user@$server_ip-p $port -i $tempKeyPath -o \"StrictHostKeyChecking=no\" -- whoami"
         );
-        if ( $result->successful() ) {
-            logger()->info( "Successfully connected to server $server_name ($server_ip) as $user." );
+        if ($result->successful()) {
+            logger()->info("Successfully connected to server $server_name ($server_ip) as $user.");
         } else {
-            logger()->error( "Failed to connect to server $server_name ($server_ip) as $user. Error: " . $result->errorOutput() );
+            logger()->error("Failed to connect to server $server_name ($server_ip) as $user. Error: ".$result->errorOutput());
         }
         // remove the temporary key file
         unlink($tempKeyPath);
---
+++ app/Services/Ansible.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Server;
+use Illuminate\Support\Facades\Process;
+use Symfony\Component\Yaml\Yaml;
+
+class Ansible
+{
+    public function GenerateInventory()
+    {
+        // Get all servers.
+        $servers = Server::all();
+        $formattedData = [
+            'all' => [
+                'hosts' => [],
+            ],
+        ];
+        // Structure the data.
+
+        foreach ($servers as $server) {
+            $formattedData['all']['hosts']['server-'.$server->id.'-'.$server->name] = [
+                'ansible_host' => $server->ip_address,
+                'ansible_user' => 'root', // @todo make this a variable.
+                'ansible_port' => 22, // @todo make this a variable.
+                'ansible_ssh_private_key_file' => $this->createTempPrivateKeyFile($server),
+            ];
+        }
+        // Create Yaml Inventory file.
+        $yaml = Yaml::dump($formattedData, 2);
+        file_put_contents('/tmp/ansible-sword-inventory.yml', $yaml);
+    }
+
+    /**
+     * Create the private key file in the temp directory and return the path.
+     *
+     * @param  $server  The Server model instance.
+     */
+    public function createTempPrivateKeyFile($server): string
+    {
+        $private_key = $server->ssh_private_key;
+
+        // Put the private key in a temporary file
+        $tempKeyPath = tempnam(sys_get_temp_dir(), 'ssh_key_'.$server->name);
+        file_put_contents($tempKeyPath, $private_key);
+        chmod($tempKeyPath, 0600); // Set permissions to read/write for the owner only.
+
+        return $tempKeyPath;
+    }
+
+    /**
+     * Run a specific playbook.
+     *
+     * @param  mixed  $serverID  the server ID, or empty to run for all servers.
+     * @param  string|null  $playbookpath  Path to a playbook, relative to ./ansible-playbooks.
+     *                                     Or full paths. Default to main.yml
+     */
+    public function RunPlaybook($server = null, ?string $playbookpath = null)
+    {
+        // Always make sure we have a fresh inventory.
+        $this->GenerateInventory();
+
+        if(is_int($server)) {
+            $server = Server::findOrFail($server);
+        }
+        $LimitServer = 'all';
+        if($server) {
+            $LimitServer = 'server-'.$server->id.'-'.$server->name;
+        }
+
+        if(empty($playbookpath)) {
+            $playbookpath = __DIR__ .'/../../ansible-playbooks/main.yml';
+        } elseif (!str_starts_with($playbookpath, '/')) {
+            $playbookpath = __DIR__ .'/../../ansible-playbooks/'.$playbookpath;
+        }
+
+        $result = Process::run("ansible-playbook -i /tmp/ansible-sword-inventory.yml $playbookpath --limit $LimitServer");
+
+        if ($result->successful()) {
+            logger()->info("Successfully ran Ansible playbook $playbookpath for server $LimitServer.");
+        } else {
+            logger()->error("Failed to run Ansible playbook $playbookpath for server $LimitServer. Error: ".$result->errorOutput());
+        }
+        echo $result->output();
+    }
+}
